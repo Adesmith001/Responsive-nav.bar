a8caed946a45f90023a68771ee3879274121e1fa
Adesmith001 <somadetoluwani@gmail.com>
2023-08-18 08:14:49 +0100

    Made responsive nav bar

---
+++ script.js
@@ -0,0 +1,7 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const navbarMenu = document.querySelector(".navbar-menu");
+
+mobileMenu.addEventListener("click", () => {
+    navbarMenu.classList.toggle("active");
+    mobileMenu.classList.toggle("active");
+});
